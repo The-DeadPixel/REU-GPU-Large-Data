df2f01a67bd4c7d54046766b4b2cfb8165f6710a
Luke Burford <lburford@rams.colostate.edu>
2018-04-19 15:09:31 -0600

    Update TilingMatrix.cu
    Works as far as I can tell.

--- Code/deviceQuery/TilingMatrix.cu
+++ Code/deviceQuery/TilingMatrix.cu
@@ -1,10 +1,244 @@
+/*
+ *  file name: TilingMatrix.cu
+ *  NOTE: 
+ *       squareMatrixMult is much more efficent than the regular multiplier
+ *       currently compiling with: nvcc TilingMatrix.cu -o tileTest
+ */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 #include <sys/time.h>
 #include <stdlib.h>
 
 #include <sys/time.h>     //measuring performance data
 
+#define BLOCK_SIZE 16
+
+/**********************************************************************
+function name: matrixTriUpper
+description: sets a matrix to an upper bound triangle matrix
+parameters: 
+            &a GPU device pointer to a m X n matrix (A)
+Note:
+    
+return: none
+**********************************************************************/
+__global__ void matrixTriUpper(int *a, int m, int n) {
+    // printf("[%d][%d]:%d == [%d][%d]:%d, ", i, j, copyC[i*k + j], i, j, copyC[i*k + j]); REMOVE THIS LATER
+    //setting matricies to their upper bound 
+    for(int i = 0; i < m; ++i) {
+        for(int j = 0; j < n; ++j) {
+            if(i>j)
+                a[i*n + j] = 0;
+            a[i*n + j] = a[i*n + j];
+        }
+    }
+}
+
+/**********************************************************************
+function name: matrixMult
+description: dot product of two matrix (not only square)
+parameters: 
+            &a GPU device pointer to a m X n matrix (A)
+            &b GPU device pointer to a n X k matrix (B)
+            &c GPU device output purpose pointer to a m X k matrix (C) 
+            to store the result
+Note:
+    grid and block should be configured as:
+        dim3 dimGrid((k + BLOCK_SIZE - 1) / BLOCK_SIZE, (m + BLOCK_SIZE - 1) / BLOCK_SIZE);
+        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    further sppedup can be obtained by using shared memory to decrease global memory access times
+return: none
+**********************************************************************/
+__global__ void matrixMult(int *a,int *b, int *c, int m, int n, int k)
+{ 
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int sum = 0;
+    if( col < k && row < m) 
+    {
+        for(int i = 0; i < n; i++) 
+        {
+            sum += a[row * n + i] * b[i * k + col];
+        }
+        c[row * k + col] = sum;
+    }
+} 
+
+/*
+*********************************************************************
+function name: squareMatrixMult
+description: dot product of two matrix (not only square) in GPU
+parameters: 
+            &a GPU device pointer to a n X n matrix (A)
+            &b GPU device pointer to a n X n matrix (B)
+            &c GPU device output purpose pointer to a n X n matrix (C) 
+            to store the result
+Note:
+    grid and block should be configured as:
+        dim3 dim_grid((n - 1) / BLOCK_SIZE + 1, (n - 1) / BLOCK_SIZE + 1, 1);
+        dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
+    SQUARE IS MUCH MORE EFFICENT THAN REGULAR
+return: none
+*********************************************************************
+*/
+__global__ void squareMatrixMult(int *d_a, int *d_b, int *d_result, int n) 
+{
+    __shared__ int tile_a[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ int tile_b[BLOCK_SIZE][BLOCK_SIZE];
+
+    int row = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+    int col = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    int tmp = 0;
+    int idx;
+
+    for (int sub = 0; sub < gridDim.x; ++sub) 
+    {
+        idx = row * n + sub * BLOCK_SIZE + threadIdx.x;
+        if(idx >= n*n)
+        {
+            // n may not divisible by BLOCK_SIZE
+            tile_a[threadIdx.y][threadIdx.x] = 0;
+        }
+        else
+        {
+            tile_a[threadIdx.y][threadIdx.x] = d_a[idx];
+        }
+
+        idx = (sub * BLOCK_SIZE + threadIdx.y) * n + col;
+        if(idx >= n*n)
+        {
+            tile_b[threadIdx.y][threadIdx.x] = 0;
+        }  
+        else
+        {
+            tile_b[threadIdx.y][threadIdx.x] = d_b[idx];
+        }
+        __syncthreads();
+
+        for (int k = 0; k < BLOCK_SIZE; ++k) 
+        {
+            tmp += tile_a[threadIdx.y][k] * tile_b[k][threadIdx.x];
+        }
+        __syncthreads();
+    }
+    if(row < n && col < n)
+    {
+        d_result[row * n + col] = tmp;
+    }
+}
+
+/**********************************************************************
+function name: main
+description: test and compare
+parameters: 
+            none
+return: none
+**********************************************************************/
+
+int main(int argc, char** argv) {
+    int printAllMat = 1; // debug flag for printing all of the maticies
+    // Set sizes of the matrixes
+    int m=10;
+    int n=10;
+    int k=10;
+    
+    /* Fixed seed for illustration */
+    srand(3333);
+
+    // Allocate memory in host RAM
+    int *copyA, *copyB, *copyC;
+    cudaMallocHost((void **) &copyA, sizeof(int)*m*n); // copied matrix is m x n
+    cudaMallocHost((void **) &copyB, sizeof(int)*n*k); // copied matrix is n x k
+    cudaMallocHost((void **) &copyC, sizeof(int)*m*k); // copied matrix is m x k
+    
+    // random initialize matrix A
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            copyA[i * n + j] = rand() % 1024;
+        }
+    }
+
+    // random initialize matrix B
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            copyB[i * k + j] = rand() % 1024;
+        }
+    }
+    
+    // Allocate memory space on the device 
+    int *matA, *matB, *matC;
+    cudaMalloc((void **) &matA, sizeof(int)*m*n); // matrix is m x n
+    cudaMalloc((void **) &matB, sizeof(int)*n*k); // matrix is n x k
+    cudaMalloc((void **) &matC, sizeof(int)*m*k); // matrix is m x k
+    
+    // copy matrix A and B from host to device memory
+    cudaMemcpy(matA, copyA, sizeof(int)*m*n, cudaMemcpyHostToDevice);
+    cudaMemcpy(matB, copyB, sizeof(int)*n*k, cudaMemcpyHostToDevice);
+
+    unsigned int grid_rows = (m + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    unsigned int grid_cols = (k + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    dim3 dimGrid(grid_cols, grid_rows);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    
+    // Launch kernel, check if it is a square
+    if(m == n && n == k) {
+        matrixTriUpper<<<dimGrid, dimBlock>>>(matA, m, n);
+        matrixTriUpper<<<dimGrid, dimBlock>>>(matB, n, k);
+        squareMatrixMult<<<dimGrid, dimBlock>>>(matA, matB, matC, n); // square, thus only need 1 param to define size
+    }
+    else { // not a square, thus it needs param to define all sizes
+        matrixMult<<<dimGrid, dimBlock>>>(matA, matB, matC, m, n, k);
+    }
+    
+    // Transefr results from device to host 
+    cudaMemcpy(copyC, matC, sizeof(int)*m*k, cudaMemcpyDeviceToHost);
+    cudaThreadSynchronize();
+    
+    //prints the matricies
+    // printf("[%d][%d]:%d, ", i, j, copyC[i*k + j]); //Another possible way to print the matrix
+    int i,j;
+    //if the debug flag is on it will print the first two product arrays as well
+    if(printAllMat == 1) {
+        // print matrix A
+        printf("matA matrix: \n");
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                //printf("[%d][%d]:%d, ", i, j, copyA[i*k + j]);
+                printf("%d ", copyA[i*k + j]);
+            }
+            printf("\n");
+        }
+        // print matrix B
+        printf("\nmatB matrix: \n");
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < k; j++) {
+                //printf("[%d][%d]:%d, ", i, j, copyB[i*k + j]);
+                printf("%d ", copyB[i*k + j]);
+            }
+            printf("\n");
+        }
+    }
+    
+    // print result matrix
+    printf("\nResult matrix: \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < k; j++) {
+                //printf("[%d][%d]:%d, ", i, j, copyC[i*k + j]);
+                printf("%d ", copyC[i*k + j]);
+            }
+        printf("\n");
+    }
+    
+    // free memory
+    cudaFree(matA);
+    cudaFree(matB);
+    cudaFree(matC);
+    cudaFreeHost(copyA);
+    cudaFreeHost(copyB);
+    cudaFreeHost(copyC);
+    return 0;
+}
