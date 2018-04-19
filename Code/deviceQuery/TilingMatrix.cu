d510518c0c70ccfbcecf2cfbc5c456c2bd35cf45
Luke Burford <lburford@rams.colostate.edu>
2018-04-19 16:35:27 -0600

    optimizes the matrix square mult

--- Code/deviceQuery/TilingMatrix.cu
+++ Code/deviceQuery/TilingMatrix.cu
@@ -98,38 +98,31 @@ __global__ void squareMatrixMult(float *d_a, float *d_b, float *d_result, int n)
     float tmp = 0;
     int idx;
 
-    for (int sub = 0; sub < gridDim.x; ++sub) 
-    {
+    for (int sub = 0; sub < gridDim.x; ++sub) {
         idx = row * n + sub * BLOCK_SIZE + threadIdx.x;
-        if(idx >= n*n)
-        {
+        if(idx >= n*n) {
             // n may not divisible by BLOCK_SIZE
             tile_a[threadIdx.y][threadIdx.x] = 0;
         }
-        else
-        {
+        else {
             tile_a[threadIdx.y][threadIdx.x] = d_a[idx];
         }
 
         idx = (sub * BLOCK_SIZE + threadIdx.y) * n + col;
-        if(idx >= n*n)
-        {
+        if(idx >= n*n) {
             tile_b[threadIdx.y][threadIdx.x] = 0;
         }  
-        else
-        {
+        else {
             tile_b[threadIdx.y][threadIdx.x] = d_b[idx];
         }
         __syncthreads();
 
-        for (int k = 0; k < BLOCK_SIZE; ++k) 
-        {
+        for (int k = sub; k < BLOCK_SIZE; ++k)  {
             tmp += tile_a[threadIdx.y][k] * tile_b[k][threadIdx.x];
         }
         __syncthreads();
     }
-    if(row < n && col < n)
-    {
+    if(row < n && col < n) {
         d_result[row * n + col] = tmp;
     }
 }
