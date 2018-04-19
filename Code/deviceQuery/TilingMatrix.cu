558ec40d307b845350df61eef453c71897304450
susMoose <35513177+susMoose@users.noreply.github.com>
2018-04-19 13:01:26 -0600

    Update TilingMatrix.cu

--- Code/deviceQuery/TilingMatrix.cu
+++ Code/deviceQuery/TilingMatrix.cu
@@ -1,1 +1,10 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+#include <sys/time.h>     //measuring performance data
+
